21b132af01c3bd7afd8efea4efe35a8ad5ae9045
Ilya Novikau <ilya.nvu@gmail.com>
2018-11-07 13:10:51 +0300

    Modify routing

--- assignment1/index.js
+++ assignment1/index.js
@@ -2,16 +2,20 @@ const http = require('http');
 const url = require('url');
 
 const routes = {
-    home: {
-        code: 200,
-        payload: {
-            welcome_message: 'Welcome!'
+    home: () => {
+        return {
+            code: 200,
+            payload: {
+                welcome_message: 'Welcome!'
+            }
         }
     },
-    not_found: {
-        code: 404,
-        payload: {
-            error: 'Not Found'
+    not_found: () => {
+        return {
+            code: 404,
+            callback: {
+                error: 'Not Found'
+            }
         }
     }
 };
@@ -20,7 +24,8 @@ const httpServer = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
     const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/^\/|\/$/g, '');
-    const { code, payload } = routes[trimmedPath] || routes.not_found;
+    const handler = routes[trimmedPath] || routes.not_found;
+    let { code, payload } = handler();
     res.statusCode = code;
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(payload));
